aa9d5f02519e167ae779c5f926dd474a7f546c64
ModDayHippie <48167631+ModDayHippie@users.noreply.github.com>
2021-01-07 10:10:48 -0500

    Update weightconvert.py
    fixed grammer

--- weightconvert.py
+++ weightconvert.py
@@ -1,12 +1,14 @@
-we = input('how much is the weight')
+we = input('how much is the weight:')
 
-wtype = input("kg or lbs")
+wtype = input("kg or lbs:")
 
 if wtype == 'lbs':
     weight = int(we) * 0.45
-    print('so your the weight is ')
+    print('so your the weight in Kg is: ')
     print(weight)
 if wtype == 'kg':
     weight = int(we) * 2.204
-    print('so the weight it')
+    print('so the weight in LBS is:')
     print(weight)
+
+input('Press Enter to Exit')
